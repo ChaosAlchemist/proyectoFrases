446b7a69591a85af14b4e00cf492eb2bb2ce1d45
ChaoticTale <jrcortest1@gmail.com>
2016-06-06 15:00:38 -0300

    Detallito

--- src/bd/Data.java
+++ src/bd/Data.java
@@ -14,27 +14,34 @@ import data.*;
  * @author Emilio
  */
 public class Data {
+
     private Conexion c;
     private String q;
-    private ResultSet rs;    
-    
-    public Data() throws SQLException{
+    private ResultSet rs;
+
+    public Data() throws SQLException {
         c = new Conexion(
-            "localhost", //servidor
-            "db_frases", //base de datos 
-            "root", //permiso
-            "123456" //clave
+                "localhost", //servidor
+                "db_frases", //base de datos 
+                "root", //permiso
+                "123456" //clave
         );
     }
 
-    public void insertar(Object o){
-        if(o instanceof Personaje){
-            q = "insert into personajes values (null,'"+((Personaje) o).getNombre()+"');";
-        }else if(o instanceof Accion){
-            q = "insert into accion values (null,'"+((Accion) o).getAccion()+"');";
-        }else{
-            q = "insert into lugar values (null,'"+((Lugar) o).getLugar()+"');";
+    public void insertar(Object o) throws SQLException {
+        if (o instanceof Personaje) {
+            q = "insert into personajes values (null,'" + ((Personaje) o).getNombre() + "');";
+        } else if (o instanceof Accion) {
+            q = "insert into accion values (null,'" + ((Accion) o).getAccion() + "');";
+        } else {
+            q = "insert into lugar values (null,'" + ((Lugar) o).getLugar() + "');";
         }
+
+        c.ejecutar(q);
     }
-    
+
+    public String generarFrase() {
+        q = "select "
+    }
+
 }
