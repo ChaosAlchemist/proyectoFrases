24cccd231e8d9cfda91183cac2ad5fdc5e0b7e23
ChaoticTale <jrcortest1@gmail.com>
2016-06-06 17:29:00 -0300

    Base de Datos Final

--- script.sql
+++ script.sql
@@ -18,27 +18,3 @@ create table lugares(
     lugar varchar(50),
     primary key(id)
 );
-
-/* create table frases(
-	id int auto_increment,
-    idPersonaje int,
-    idAccion int,
-    idLugar int,
-    frase text,
-    foreign key(idPersonaje) references personajes(id),
-    foreign key(idAccion) references accion(id),
-    foreign key(idLugar) references lugar(id),
-    primary key(id)
-); */
-
-
-insert into personajes values (null,'Emilio');
-insert into personajes values (null,'José');
-
-insert into acciones values (null,'está comiendo');
-insert into acciones values (null,'está durmiendo');
-
-insert into lugares values (null,'en la Playa');
-insert into lugares values (null,'en el Cerro');
-
-select max(id) from personajes;
